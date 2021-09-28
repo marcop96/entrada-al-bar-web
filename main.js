3a2b81a810128c2fb5c2f4b774a37244345557d2
marcop96 <55959162+marcop96@users.noreply.github.com>
2021-09-28 21:18:06 +0200

    agrega nombre al saludo
    corrige errores y limpia un poco esteticamente (está horrible ya se)

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const botonConfirmar = document.querySelector(".boton-confirmar")
 
 botonConfirmar.onclick = function(){
 let respuestaEntrada;
-const edadUsuario = Number(document.querySelector(".edad-usuario")) //acá no me deja poner value/innertext
+const nombreUsuario = document.querySelector(".nombre-usuario").value
+const edadUsuario = Number(document.querySelector(".edad-usuario").value) //acá no me deja poner value/innertext
 if(edadUsuario>=18){
 respuestaEntrada= "bienvenido"
 }
 else{respuestaEntrada= "no podés pasar"}
+document.querySelector(".respuesta-entrada").innerText = `Hola ${nombreUsuario}, ${respuestaEntrada}`
 
 return false
 }
