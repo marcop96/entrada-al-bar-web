422cccab02799ebdd14697277a45d248919c8137
marcop96 <55959162+marcop96@users.noreply.github.com>
2021-09-29 15:01:52 +0200

    agrega funcionalidad al botón confirmar
    Agrega respuesta por si el usuario no ingresa ningun dato

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const botonConfirmar = document.querySelector(".boton-confirmar")
 
-botonConfirmar.onclick = function(){
-let respuestaEntrada;
-const nombreUsuario = document.querySelector(".nombre-usuario").value
-const edadUsuario = Number(document.querySelector(".edad-usuario").value) //acá no me deja poner value/innertext
-if(edadUsuario>=18){
-respuestaEntrada= "bienvenido"
-}
-else{respuestaEntrada= "no podés pasar"}
-document.querySelector(".respuesta-entrada").innerText = `Hola ${nombreUsuario}, ${respuestaEntrada}`
+botonConfirmar.onclick = function () {
+    let respuestaEntrada;
+    const nombreUsuario = document.querySelector(".nombre-usuario").value
+    const edadUsuario = Number(document.querySelector(".edad-usuario").value)
+    if (edadUsuario == "") {
+        respuestaEntrada = "no ingresaste nada!"
+    }
+    else if (edadUsuario >= 18) {
+        respuestaEntrada = "bienvenido al bar"
+    }
+    else if (edadUsuario < 18) { respuestaEntrada = "no podés pasar" }
 
-return false
+    document.querySelector(".respuesta-entrada").innerText = `Hola ${nombreUsuario}, ${respuestaEntrada}`
+
+    return false
 }
+
+/*botón resetear
+ cambia el .respuesta entrada a vacio
+*/
+
